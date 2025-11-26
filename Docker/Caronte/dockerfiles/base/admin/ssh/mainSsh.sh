f70f9f87030b889512b9804873b743a7b13fdb1f
Oliver <dangelomagallanes1@gmail.com>
2025-11-26 12:02:04 +0100

    Refactor SSH configuration and add sudoers setup in mainSsh.sh; update Dockerfile and docker-compose.yml for improved service management

--- Docker/Caronte/dockerfiles/base/admin/ssh/mainSsh.sh
+++ Docker/Caronte/dockerfiles/base/admin/ssh/mainSsh.sh
@@ -2,21 +2,29 @@
 #!/bin/bash
 set -e
 
-make_ssh(){
-    # Crear directorio para SSH si no existe
-    mkdir -p /var/run/sshd
-    
-    # Configurar SSH
-    sed -i 's/#Port 22/Port 2345/' /etc/ssh/sshd_config
+config_ssh(){
     sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
-    sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/' /etc/ssh/sshd_config
-    sed -i 's/#AuthorizedKeysFile/AuthorizedKeysFile/' /etc/ssh/sshd_config
-    
-    # Iniciar servicio SSH
-    service ssh start
-    
-    echo "--> SSH configurado en puerto 2345" >> /root/logs/informe.log
+    sed -i 's/#Port 22/Port 22/' /etc/ssh/sshd_config
+    if [ ! -d /home/${USUARIO}/.ssh ]
+    then
+        mkdir /home/${USUARIO}/.ssh
+        cat /home/${USUARIO}/devops/HLC/Docker/Caronte/common/id_ed25519.pub >> /home/${USUARIO}/.ssh/authorized_keys
+    fi
+    # /etc/init.d/ssh start &
+    exec /usr/sbin/sshd -D & # dejar el ssh en background (2plano)
 }
 
-# exec /etc/init.d/ssh start &
-# exec /usr/sbin/sshd -D & # Dejar el ssh en segundo plano 
+config_sudoers(){
+    if [ -f /etc/sudoers ]
+    then 
+        #comprobar que el ${USUARIO} No existe
+        echo "${USUARIO} ALL=(ALL:ALL) ALL" >> /etc/sudoers
+    fi
+}
+
+newSSH(){
+    #gestion de errores y salida a logs
+    
+    config_sudoers
+    config_ssh
+}
