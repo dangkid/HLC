aec6f8d8f93f60a57b3961c6d653748394af52d9
Oliver <dangelomagallanes1@gmail.com>
2025-12-02 13:21:21 +0100

    Add Dockerfile, start.sh, .env, and docker-compose.yml for nginx setup; include logging configuration

---
+++ Docker/Caronte/dockerfiles/base/sweb/nginx/admin/start.sh
@@ -0,0 +1,26 @@
+config_nginx(){
+#inicia nginx en segundo plano
+#para lanzar nginx en segundo plano y mantener el ocntenedor activo, n
+# necesitas un proceso en primer plano que evite que el docker finalice el contenedor
+# nginx, por defecto, se ejecuta como un demonio (en segundo plano),
+#pero Docker requiere un proceso principal activo en el contenedor
+
+nginx &
+# mantener el contendor activo ejecutando nginx en primer plano
+#exec nginx -g 'daemon off;'
+#manten el contenedor activo
+#tail -f /dev/null
+
+}
+
+load_entrypoint_base(){
+#ejecutar el entrypoint ubbase    
+bash /root/admin/base/start.sh
+}
+
+main(){
+    load_entrypoint_base
+    config_nginx
+
+    tail -f /dev/null
+}
